ad2cfec258d661a87849d68b9aefa66d7aecacd9
n.kobyakov <n.kobyakov@21-school.ru>
2025-06-11 02:07:21 +0300

    Добавил линтеры

---
+++ backend/eslint.config.js
@@ -0,0 +1,55 @@
+const globals = require("globals");
+const js = require('@eslint/js');
+const tsESLint = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+const importPlugin = require('eslint-plugin-import');
+const prettier = require('eslint-plugin-prettier');
+
+module.exports = [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    plugins: {
+      '@typescript-eslint': tsESLint,
+      'import': importPlugin,
+      'prettier': prettier
+    },
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: true
+      },
+      globals: {
+        ...globals.node, // Подключает стандартные Node.js глобалы (console, process, etc.)
+        require: 'readonly', // Добавьте require как глобальную переменную
+        module: 'readonly'    // Добавьте module как глобальную переменную
+      },
+    },
+    rules: {
+      ...tsESLint.configs.recommended.rules,
+      'prettier/prettier': 'error',
+      'no-unused-vars': 'warn',
+      'import/order': [
+        'error',
+        {
+          'groups': [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index'
+          ],
+          'newlines-between': 'always',
+          'alphabetize': {
+            'order': 'asc',
+            'caseInsensitive': true
+          }
+        }
+      ],
+      '@typescript-eslint/consistent-type-imports': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'quotes': ['error', 'single', { 'avoidEscape': true }]
+    }
+  }
+];
